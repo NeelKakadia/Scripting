3112fc7b087d3242e2383f0629313a2f5483c4b3
Neel Kakadia <neelkakadia@1a:e2:a0:73:3c:9d.home>
2026-02-10 17:36:16 -0500

    modified variables file

--- variables.sh
+++ variables.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 << comment 
-playing with the variables
-comment 
+playing with the variables 
+comment
 
 name="neel"
 
 echo "name is $name"
+
+echo "The directory is $(pwd)"
+echo "Today's date is $(date)"
+
+
+ 
