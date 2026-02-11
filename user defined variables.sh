b1e5ddd53d18a90352bfb26f4416f459dd795926
Neel Kakadia <neelkakadia@1a:e2:a0:73:3c:9d.home>
2026-02-11 15:51:20 -0500

    User Defined Variables added

---
+++ user defined variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "It is a script for user defined variable"
+
+read -p "Enter Username:" username
+
+
+<< comment
+or other way is echo "Enter the username"
+read username
+comment
+
+echo "username is $username"
