a9a064198158825906e752eda7ee4524e08563c3
Neel Kakadia <neelkakadia@1a:e2:a0:73:3c:9d.home>
2026-02-11 18:23:41 -0500

    udf script modified

--- user defined variables.sh
+++ user defined variables.sh
@@ -11,3 +11,13 @@ read username
 comment
 
 echo "username is $username"
+
+<< cm1
+taking input from the arguments
+you can use this method to create multiple user accounts without passing it as a user defiend argument
+cm1
+
+echo "the username is $1"
+
+
+
